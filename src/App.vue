6fc8a09e03f828487168f51ced9170c55112f023
ahmed_alashii <you@example.com>
2023-03-27 10:25:56 +0200

    First Vue Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
+<!-- .vue files are single file components -->
+<!--  Vue Single-File Components (a.k.a. *.vue files, abbreviated as SFC) is a special file format that allows us to encapsulate the template, logic, and styling of a Vue component in a single file. -->
+<!-- .vue files allows us to write our components in a single file, which is a great way to organize our code. -->
+<!-- .vue files allows us to write vue apps (components specifically) in a much nicer, modular, and organized way -->
+<!-- .vue files allows us to split our template (the html code that belongs to an app or component), the script part, and the style part into three different sections. -->
+<!--  SFC files are files that contain three different sections: template, script, and styles. (html, javascript, and css) --> 
+<!--  The template section allows us to write our html code for our app or component. -->
+<!--  The script section allows us to write our javascript code for our app or component. -->
+<!--  The styles section allows us to write our css code for our app or component. -->
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
@@ -14,6 +24,7 @@ export default {
 }
 </script>
 
+<!--  styles section allows us to write our css code for our app or component. -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,3 +35,8 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+<!-- Build workflow -->
+<!-- It's important to note that the .vue files are not supported by the browser. -->
+<!-- The browser only understands javascript, so we need to compile our .vue files into javascript files that the browser can understand. -->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+// main.js file is the main entry point of the application and it is the first file that is executed when the application is loaded.
+// It is used to import the Vue library and the App component and mount the App component to the DOM.
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// ; is not required in JavaScript but it is a good practice to use it.
 createApp(App).mount('#app')
+// #app is the id of the div element in the public/index.html file.
